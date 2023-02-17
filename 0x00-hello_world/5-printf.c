b98316db3a614b0bb38958aa91b66c352a20bc1d
IvyMachogu <IvyMachogu>
2023-02-17 11:26:40 -0800

    6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 /**
  * main - prints "with proper grammar,but the outcome is 
  * a piece of art," followed newline
- * return:always 0.
+ * Return: always 0.
  *
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf ("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
