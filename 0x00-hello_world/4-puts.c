643afd153ff7daeb4eba68edbc45b1dd52f14b52
IvyMachogu <IvyMachogu>
2023-02-17 11:47:33 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
-*main-Entry
-*Return:Always 0 
-*/
+ * main - prints "programming is like building a multilingual
+ * puzzle, followed by a newline."
+ * Return 0 .
+ */
 int main(void)
 {
-puts(" \ " Programming is like building a multilingual puzzle ");
-	return (0);
+	puts("\""Programming is like building a multilingual puzzle");
+	retur (0);
 }
