0abe7bdded2171449150c7df106c6a4079c95712
IvyMachogu <IvyMachogu>
2023-03-11 00:14:21 -0800

    0

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 
 int main(void)
@@ -6,4 +5,3 @@ int main(void)
     _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
     return (0);
 }
-
