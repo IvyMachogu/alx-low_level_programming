98cf23e4003e5988f6bcd664f5fa75744d0622db
IvyMachogu <IvyMachogu>
2023-03-14 04:46:24 -0700

    3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,6 +14,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	mee = malloc(sizeof(int *) * height);
+
 	if (mee == NULL)
 		return (NULL);
 	for (x = 0; x < height; x++)
@@ -27,12 +28,11 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 	}
+
 	for (x = 0; x < height; x++)
 	{
-
 		for (y = 0; y < width; y++)
-
-				mee[x][y] = 0;
+			mee[x][y] = 0;
 
 	}
 	return (mee);
