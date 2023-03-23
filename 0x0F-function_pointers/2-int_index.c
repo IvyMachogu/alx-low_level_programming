277f4d4b862b4529cc7275a30cc9afef3a8a85b0
IvyMachogu <IvyMachogu>
2023-03-23 10:28:55 -0700

    a program that performs simple operations.

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,20 +4,22 @@
  *  @array: array.
  *  @size: size of elements in array.
  *  @cmp: pointer to func of one of the 3 in main.
- *  Return: 0.
+ *  Return : 0.
+ *
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || size <= 0 || cmp == NULL)
-		return (-1);
+	if (array == NULL || size <= 0 || cmp == NULL
+			return (-1);
 
-	for (i = 0; i < size; i++)
-	{
-		if (cmp(array[i]))
+			for (i = 0; i < size; i++)
+			{
+
+			if (cmp(array[i]))
 			return (i);
-
 	}
-	return(-i);
+              return (-1);
 }
+			   
