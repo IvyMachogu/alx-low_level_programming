ece0d2c0891fa32463120a3802477907839ed713
IvyMachogu <IvyMachogu>
2023-03-24 04:28:50 -0700

    a function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,58 +1,53 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
+/**
+ *  print_all - prints anything.
+ *  @format: list of types of arguments passed to the function.
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char *str, *sep = "";
 
 
-	/**
-	 * print_all - prints anything
-	 * @format: list of types of arguments passed to the function
-	 */
-	void print_all(const char * const format, ...)
+	va_list list;
+
+	va_start(list, format);
+
+	if (format)
 	{
-		int i = 0;
-		char *str, *sep = "";
-
-
-		va_list list;
-
-
-		va_start(list, format);
-
-
-		if (format)
+		while (format[i])
 		{
-			while (format[i])
+			switch (format[i])
 			{
-				switch (format[i])
-				{
-					case 'c':
-						printf("%s%c", sep, va_arg(list, int));
+				case 'c':
+					printf("%s%c", sep, va_arg(list, int));
 						break;
-					case 'i':
+						case 'i':
 						printf("%s%d", sep, va_arg(list, int));
 						break;
-					case 'f':
+						case 'f':
 						printf("%s%f", sep, va_arg(list, double));
 						break;
-					case 's':
+						case 's':
 						str = va_arg(list, char *);
 						if (!str)
-							str = "(nil)";
+							str="(nil)";
 						printf("%s%s", sep, str);
 						break;
-					default:
+						default:
 						i++;
 						continue;
-				}
-				sep = ", ";
-				i++;
 			}
+			sep = ", ";
+			i++;
 		}
-
-
-		printf("\n");
-		va_end(list);
 	}
+	printf("\n");
+	va_end(list);
+}
+
 
 
 
