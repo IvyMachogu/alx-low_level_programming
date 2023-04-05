8847556af407d7c4790278888b14d7c45e58619e
IvyMachogu <IvyMachogu>
2023-04-05 11:50:51 -0700

    a function that reverses a listint_t linked list.

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * delete_nodeint_at_index - deletes a node in a linked list at a certain index.
+ * delete_nodeint_at_index - delete a node in  linked list at a certain index.
  * @head: pointer to the first element in the list.
  * @index: index of the node to delete.
  * Return: 1 success,  -1 fail.
