e4cee7ba60f1ca55ccbeb1edd26cb99e8ce17f45
IvyMachogu <IvyMachogu>
2023-04-05 11:40:07 -0700

    a function that reverses a listint_t linked list.

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -11,9 +11,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *current = NULL;
 	unsigned int i = 0;
 
-	if(*head == NULL)
-		return(-1);
-	if(index == 0)
+	if (*head == NULL)
+		return (-1);
+	if (index == 0)
 	{
 
 		*head = (*head) -> next;
@@ -21,7 +21,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return(1);
 	}
 	while (i < index -1)
-	{if(!temp || !(temp -> next))
+	{
+		if (!temp || !(temp -> next))
 		return (-1);
 		temp = temp -> next;
 		i++;
